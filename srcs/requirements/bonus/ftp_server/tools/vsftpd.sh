1c9d86d485a8ef21e0c0f517413da5ed57807e5d
claire <ccarnot@student.42.fr>
2024-06-12 18:26:40 +0200

    wip2

--- srcs/requirements/bonus/ftp_server/tools/vsftpd.sh
+++ srcs/requirements/bonus/ftp_server/tools/vsftpd.sh
@@ -4,7 +4,7 @@
 if [ ! -f "/etc/vsftpd_config" ]; then
 
     useradd -m $FTP_USR && echo "$FTP_USR:$FTP_PASSWORD" | chpasswd
-    chown -R $FTP_USR:$FTP_USR /var/www/html
+    chown -R $FTP_USR:$FTP_USR /var/www/ccarnot
 
     mkdir -p /var/run/vsftpd/empty
 
